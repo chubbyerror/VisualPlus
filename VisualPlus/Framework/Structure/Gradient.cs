a38af958ce65762d3901d07e2e24c6fa3f1b2e48
DarkByte7 <DarkByte7@Live.com>
2017-05-22 12:09:58 -0500

    Fixed Gradient type

--- VisualPlus/Framework/Structure/Gradient.cs
+++ VisualPlus/Framework/Structure/Gradient.cs
@@ -7,37 +7,51 @@
     using System.Drawing;
     using System.Globalization;
 
+    using VisualPlus.Localization;
+
     #endregion
 
     [TypeConverter(typeof(GradientConverter))]
+    [Description(Localize.Description.Border)]
     public class Gradient
     {
         #region Variables
 
-        private Color[] colors;
-        private float[] positions;
+        private float angle;
+        private Color[] colors =
+            {
+                Color.Red,
+                Color.Green,
+                Color.Blue
+            };
 
-        #endregion
-
-        #region Constructors
-
-        /// <summary>The gradient.</summary>
-        /// <param name="colors">The gradients colors.</param>
-        /// <param name="positions">The gradients positions.</param>
-        /// <returns>The <see cref="Gradient" />.</returns>
-        public Gradient(Color[] colors, float[] positions)
-        {
-            Colors = colors;
-            Positions = positions;
-        }
+        private float[] positions = { 0, 1 / 2f, 1 };
+        private Point endPoint;
+        private Point startPoint;
 
         #endregion
 
         #region Properties
 
-        [RefreshProperties(RefreshProperties.Repaint)]
         [NotifyParentProperty(true)]
-        [DefaultValue(typeof(Color), "LightBlue")]
+        [RefreshProperties(RefreshProperties.Repaint)]
+        [Description("The gradient angle.")]
+        public float Angle
+        {
+            get
+            {
+                return angle;
+            }
+
+            set
+            {
+                angle = value;
+            }
+        }
+
+        [NotifyParentProperty(true)]
+        [RefreshProperties(RefreshProperties.Repaint)]
+        [Description("The gradient colors.")]
         public Color[] Colors
         {
             get
@@ -48,13 +62,12 @@
             set
             {
                 colors = value;
-                OnGradientChanged(EventArgs.Empty);
             }
         }
 
-        [RefreshProperties(RefreshProperties.Repaint)]
         [NotifyParentProperty(true)]
-        [DefaultValue(typeof(float), "0")]
+        [RefreshProperties(RefreshProperties.Repaint)]
+        [Description("The color positioning of the gradient.")]
         public float[] Positions
         {
             get
@@ -65,53 +78,6 @@
             set
             {
                 positions = value;
-                OnGradientChanged(EventArgs.Empty);
-            }
-        }
-
-        #endregion
-
-        #region Events
-
-        public Gradient FromString(object value)
-        {
-            var values = ((string)value).Split(',');
-            if (values.Length != 3)
-            {
-                throw new ArgumentException("Could not convert the value");
-            }
-
-            try
-            {
-                Gradient gradient = new Gradient(colors, positions);
-
-                // Retrieve the colors.
-                ColorConverter converter = new ColorConverter();
-                gradient.Colors = (Color[])converter.ConvertFromString(values[0]);
-                gradient.Positions = (float[])converter.ConvertFromString(values[1]);
-
-                // gradient.ColorA = (Color)converter.ConvertFromString(values[0]);
-                // gradient.ColorB = (Color)converter.ConvertFromString(values[1]);
-
-                // Convert the name of the enumerated value into the corresponding
-                // enumerated value (which is actually an integer constant).
-                //gradient.GradientFillStyle = (LinearGradientMode)Enum.Parse(typeof(LinearGradientMode), values[2], true);
-
-                return gradient;
-            }
-            catch (Exception)
-            {
-                throw new ArgumentException("Could not convert the value");
-            }
-        }
-
-        public event EventHandler GradientChanged;
-
-        private void OnGradientChanged(EventArgs e)
-        {
-            if (GradientChanged != null)
-            {
-                GradientChanged(this, e);
             }
         }
 
@@ -120,93 +86,64 @@
 
     public class GradientConverter : ExpandableObjectConverter
     {
-        #region Variables
-
-        private TypeConverter converter = TypeDescriptor.GetConverter(typeof(Color));
-
-        #endregion
-
         #region Events
 
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
         {
-            if (sourceType == typeof(string))
-            {
-                return true;
-            }
-            else
-            {
-                return base.CanConvertFrom(context, sourceType);
-            }
-        }
-
-        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
-        {
-            if (destinationType == typeof(string))
-            {
-                return true;
-            }
-            else
-            {
-                return base.CanConvertTo(context, destinationType);
-            }
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
         }
 
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
-            if (value is string)
+            var stringValue = value as string;
+
+            if (stringValue != null)
             {
-                return ConvertFromString(value.ToString());
+                return new ObjectGradientWrapper(stringValue);
             }
-            else
-            {
-                return base.ConvertFrom(context, culture, value);
-            }
+
+            return base.ConvertFrom(context, culture, value);
         }
-
-        // public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-        // {
-        // if (destinationType == typeof(InstanceDescriptor) && value is GradientFill)
-        // {
-        // GradientFill gradient = (GradientFill)value;
-
-        // // Specify the three-parameter (Color-Color-LinearGradientMode)  
-        // // constructor. 
-        // ConstructorInfo ctor = typeof(GradientFill).GetConstructor(new[] { typeof(Color), typeof(Color), typeof(LinearGradientMode) });
-        // return new InstanceDescriptor(ctor, new object[] { gradient.ColorA, gradient.ColorB, gradient.GradientFillStyle });
-        // }
-        // else
-        // {
-        // return base.ConvertTo(context, culture, value, destinationType);
-        // }
-        // }
 
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
-            if (destinationType == typeof(string))
+            Gradient gradient;
+            object result;
+
+            result = null;
+            gradient = value as Gradient;
+
+            if (gradient != null && destinationType == typeof(string))
             {
-                return ConvertToString(value);
+                // result = borderStyle.ToString();
+                result = "Gradient Settings";
             }
-            else
-            {
-                return base.ConvertTo(context, culture, value, destinationType);
-            }
+
+            return result ?? base.ConvertTo(context, culture, value, destinationType);
         }
 
-        public new string ConvertToString(object value)
+        #endregion
+    }
+
+    [TypeConverter(typeof(GradientConverter))]
+    public class ObjectGradientWrapper
+    {
+        #region Constructors
+
+        public ObjectGradientWrapper()
         {
-            Gradient fill = (Gradient)value;
-            ColorConverter converter = new ColorConverter();
-
-           // return string.Format("{0}, {1}", converter.ConvertToString(fill.Colors), converter.ConvertToString(fill.Positions));
-            return "Gradient Options";
         }
 
-        public override bool GetCreateInstanceSupported(ITypeDescriptorContext context)
+        public ObjectGradientWrapper(string value)
         {
-            // Always force a new instance.    
-            return true;
+            Value = value;
         }
+
+        #endregion
+
+        #region Properties
+
+        public object Value { get; set; }
 
         #endregion
     }
