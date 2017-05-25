0a1897bc75bf7fbb4f84910360ef52390b801431
DarkByte7 <DarkByte7@Live.com>
2017-05-24 20:59:30 -0500

    Added: ButtonWidth Added: Button Direction

--- VisualPlus/Controls/VisualComboBox.cs
+++ VisualPlus/Controls/VisualComboBox.cs
@@ -31,6 +31,12 @@
         private Border border = new Border();
         private Color buttonColor = Settings.DefaultValue.Style.DropDownButtonColor;
 
+        private Direction buttonDirection = Direction.Right;
+        private DropDownButtons buttonStyles = DropDownButtons.Arrow;
+        private bool buttonVisible = Settings.DefaultValue.TextVisible;
+
+        private int buttonWidth = 30;
+
         private Color[] controlColor =
             {
                 ControlPaint.Light(Settings.DefaultValue.Style.BackgroundColor(0)),
@@ -49,8 +55,6 @@
         private Gradient controlGradient = new Gradient();
         private GraphicsPath controlGraphicsPath;
         private ControlState controlState = ControlState.Normal;
-        private DropDownButtons dropDownButton = DropDownButtons.Arrow;
-        private bool dropDownButtonsVisible = Settings.DefaultValue.TextVisible;
         private Color foreColor = Settings.DefaultValue.Style.ForeColor(0);
         private Border itemBorder = new Border();
         private Color menuItemHover = Settings.DefaultValue.Style.ItemHover(0);
@@ -60,7 +64,6 @@
         private Color separatorShadowColor = Settings.DefaultValue.Style.ShadowColor;
         private bool separatorVisible = Settings.DefaultValue.TextVisible;
         private int startIndex;
-
         private StringAlignment textAlignment = StringAlignment.Center;
         private Color textDisabledColor = Settings.DefaultValue.Style.TextDisabled;
         private TextRenderingHint textRendererHint = Settings.DefaultValue.TextRenderingHint;
@@ -165,6 +168,71 @@
             }
         }
 
+        [Category(Localize.Category.Layout)]
+        [Description(Localize.Description.Direction)]
+        public Direction ButtonDirection
+        {
+            get
+            {
+                return buttonDirection;
+            }
+
+            set
+            {
+                buttonDirection = value;
+                Invalidate();
+            }
+        }
+
+        [Category(Localize.Category.Appearance)]
+        [Description(Localize.Description.DropDownButton)]
+        public DropDownButtons ButtonStyles
+        {
+            get
+            {
+                return buttonStyles;
+            }
+
+            set
+            {
+                buttonStyles = value;
+                Invalidate();
+            }
+        }
+
+        [DefaultValue(Settings.DefaultValue.TextVisible)]
+        [Category(Localize.Category.Behavior)]
+        [Description(Localize.Description.ComponentVisible)]
+        public bool ButtonVisible
+        {
+            get
+            {
+                return buttonVisible;
+            }
+
+            set
+            {
+                buttonVisible = value;
+                Invalidate();
+            }
+        }
+
+        [Category(Localize.Category.Appearance)]
+        [Description(Localize.Description.ComponentSize)]
+        public int ButtonWidth
+        {
+            get
+            {
+                return buttonWidth;
+            }
+
+            set
+            {
+                buttonWidth = value;
+                Invalidate();
+            }
+        }
+
         [TypeConverter(typeof(GradientConverter))]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
         [Category(Localize.Category.Appearance)]
@@ -178,39 +246,6 @@
             set
             {
                 controlDisabledGradient = value;
-                Invalidate();
-            }
-        }
-
-        [Category(Localize.Category.Appearance)]
-        [Description(Localize.Description.DropDownButton)]
-        public DropDownButtons DropDownButton
-        {
-            get
-            {
-                return dropDownButton;
-            }
-
-            set
-            {
-                dropDownButton = value;
-                Invalidate();
-            }
-        }
-
-        [DefaultValue(Settings.DefaultValue.TextVisible)]
-        [Category(Localize.Category.Behavior)]
-        [Description(Localize.Description.ComponentVisible)]
-        public bool DropDownButtonVisible
-        {
-            get
-            {
-                return dropDownButtonsVisible;
-            }
-
-            set
-            {
-                dropDownButtonsVisible = value;
                 Invalidate();
             }
         }
@@ -574,13 +609,10 @@
 
             foreColor = Enabled ? foreColor : textDisabledColor;
             Gradient controlCheckTemp;
+
             if (Enabled)
             {
                 controlCheckTemp = controlGradient;
-
-                if (controlState == ControlState.Hover)
-                {
-                }
             }
             else
             {
@@ -591,51 +623,31 @@
             LinearGradientBrush gradientBackgroundBrush = GDI.CreateGradientBrush(controlCheckTemp.Colors, gradientPoints, controlCheckTemp.Angle, controlCheckTemp.Positions);
             graphics.FillPath(gradientBackgroundBrush, controlGraphicsPath);
 
-            // Create border
             if (border.Visible)
             {
                 GDI.DrawBorderType(graphics, controlState, controlGraphicsPath, border.Thickness, border.Color, border.HoverColor, border.HoverVisible);
             }
 
-            Rectangle buttonRectangle = new Rectangle(0, 0, 25, 25);
-            buttonRectangle = buttonRectangle.AlignCenterY(ClientRectangle);
-            buttonRectangle = buttonRectangle.AlignRight(ClientRectangle, 0);
+            Point textBoxPoint;
+            Point buttonPoint;
+            Size buttonSize = new Size(buttonWidth, Height);
 
-            if (dropDownButtonsVisible)
+            if (buttonDirection == Direction.Right)
             {
-                Point buttonImagePoint;
-                Size buttonImageSize;
-
-                // Draw drop down button
-                switch (dropDownButton)
-                {
-                    case DropDownButtons.Arrow:
-                        {
-                            buttonImageSize = new Size(25, 25);
-                            buttonImagePoint = new Point(buttonRectangle.X, buttonRectangle.Y);
-
-                            Arrow.DrawArrow(graphics, buttonImagePoint, buttonImageSize, buttonColor, 13);
-                            break;
-                        }
-
-                    case DropDownButtons.Bars:
-                        {
-                            buttonImageSize = new Size(18, 10);
-                            buttonImagePoint = new Point(buttonRectangle.X + 2, buttonRectangle.Y + buttonRectangle.Width / 2 - buttonImageSize.Height);
-                            Bars.DrawBars(graphics, buttonImagePoint, buttonImageSize, buttonColor, 3, 5);
-                            break;
-                        }
-                }
+                buttonPoint = new Point(Width - buttonWidth, 0);
+                textBoxPoint = new Point(0, 0);
+            }
+            else
+            {
+                buttonPoint = new Point(0, 0);
+                textBoxPoint = new Point(buttonWidth, 0);
             }
 
-            if (separatorVisible)
-            {
-                // Draw the separator
-                graphics.DrawLine(new Pen(separatorColor), buttonRectangle.X - 2, 4, buttonRectangle.X - 2, Height - 5);
-                graphics.DrawLine(new Pen(separatorShadowColor), buttonRectangle.X - 1, 4, buttonRectangle.X - 1, Height - 5);
-            }
+            Rectangle buttonRectangle = new Rectangle(buttonPoint, buttonSize);
+            Rectangle textBoxRectangle = new Rectangle(textBoxPoint.X, textBoxPoint.Y, Width - buttonWidth, Height);
 
-            Rectangle textBoxRectangle = new Rectangle(3, 0, Width - 20, Height);
+            DrawButton(graphics, buttonRectangle);
+            DrawSeparator(graphics, buttonRectangle);
 
             StringFormat stringFormat = new StringFormat
                 {
@@ -647,11 +659,14 @@
             {
                 textBoxRectangle.Width -= buttonRectangle.Width;
             }
+            else if (textAlignment == StringAlignment.Near)
+            {
+                textBoxRectangle.X += buttonWidth;
+            }
 
             graphics.DrawString(Text, Font, new SolidBrush(foreColor), textBoxRectangle, stringFormat);
 
-            // Draw the watermark
-            if (watermarkVisible && Text.Length == 0)
+            if (watermarkVisible && (Text.Length == 0))
             {
                 graphics.DrawString(waterMarkText, WaterMarkFont, waterMarkBrush, textBoxRectangle, stringFormat);
             }
@@ -660,6 +675,51 @@
         protected override void OnSelectionChangeCommitted(EventArgs e)
         {
             OnLostFocus(e);
+        }
+
+        private void DrawButton(Graphics graphics, Rectangle buttonRectangle)
+        {
+            if (buttonVisible)
+            {
+                Point buttonImagePoint;
+                Size buttonImageSize;
+
+                switch (buttonStyles)
+                {
+                    case DropDownButtons.Arrow:
+                        {
+                            buttonImageSize = new Size(25, 25);
+                            buttonImagePoint = new Point((buttonRectangle.X + (buttonRectangle.Width / 2)) - (buttonImageSize.Width / 2), (buttonRectangle.Y + (buttonRectangle.Height / 2)) - (buttonImageSize.Height / 2));
+                            Arrow.DrawArrow(graphics, buttonImagePoint, buttonImageSize, buttonColor, 13);
+                            break;
+                        }
+
+                    case DropDownButtons.Bars:
+                        {
+                            buttonImageSize = new Size(18, 10);
+                            buttonImagePoint = new Point((buttonRectangle.X + (buttonRectangle.Width / 2)) - (buttonImageSize.Width / 2), (buttonRectangle.Y + (buttonRectangle.Height / 2)) - buttonImageSize.Height);
+                            Bars.DrawBars(graphics, buttonImagePoint, buttonImageSize, buttonColor, 3, 5);
+                            break;
+                        }
+                }
+            }
+        }
+
+        private void DrawSeparator(Graphics graphics, Rectangle buttonRectangle)
+        {
+            if (separatorVisible)
+            {
+                if (buttonDirection == Direction.Right)
+                {
+                    graphics.DrawLine(new Pen(separatorColor), buttonRectangle.X - 1, 4, buttonRectangle.X - 1, Height - 5);
+                    graphics.DrawLine(new Pen(separatorShadowColor), buttonRectangle.X, 4, buttonRectangle.X, Height - 5);
+                }
+                else
+                {
+                    graphics.DrawLine(new Pen(separatorColor), buttonRectangle.Width - 1, 4, buttonRectangle.Width - 1, Height - 5);
+                    graphics.DrawLine(new Pen(separatorShadowColor), buttonRectangle.Width, 4, buttonRectangle.Width, Height - 5);
+                }
+            }
         }
 
         #endregion
