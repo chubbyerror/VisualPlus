c06baa49eb40c634795e6f98ad391f4bbffd7e4f
DarkByte7 <DarkByte7@Live.com>
2017-06-19 13:50:47 -0500

    Restored control state

--- VisualPlus/Framework/Structure/MouseState.cs
+++ VisualPlus/Framework/Structure/MouseState.cs
@@ -42,10 +42,32 @@
             control.MouseUp += OnMouseUp;
 
             // Specific controls might need to ignore some events
-            if (!(control is VisualCheckBox))
-            {
-                // Add here
-            }
+            //if (!(control is VisualCheckBox))
+            //{
+            //    // Add here
+            //}
+
+            //if (control is VisualNumericUpDown)
+            //{
+            //    control.MouseEnter += OnMouseEnter;
+            //    control.MouseLeave += OnMouseLeave;
+
+            // //   control.Invalidate();
+
+            //    // TODO: Doesn't seem to be registering the OnMouseLeave() event Invalidate() after on the control properly.
+            //}
+
+            //if (control is VisualRichTextBox)
+            //{
+            //    control.Enter += OnMouseEnter;
+            //  //  control.Leave += OnMouseLeave;
+            //}
+
+            //if (control is VisualTextBox)
+            //{
+            //    control.Enter += OnMouseEnter;
+            //   // control.Leave += OnMouseLeave;
+            //}
         }
 
         #endregion
@@ -94,68 +116,4 @@
 
         #endregion
     }
-
-    //public class MouseStateConverter : ExpandableObjectConverter
-    //{
-    //    #region Events
-
-    //    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
-    //    {
-    //        return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
-    //    }
-
-    //    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-    //    {
-    //        var stringValue = value as string;
-
-    //        if (stringValue != null)
-    //        {
-    //            return new ObjectMouseStateWrapper(stringValue);
-    //        }
-
-    //        return base.ConvertFrom(context, culture, value);
-    //    }
-
-    //    public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-    //    {
-    //        MouseState mouseState;
-    //        object result;
-
-    //        result = null;
-    //        mouseState = value as MouseState;
-
-    //        if (mouseState != null && destinationType == typeof(string))
-    //        {
-    //            // result = borderStyle.ToString();
-    //            result = "MouseState Settings";
-    //        }
-
-    //        return result ?? base.ConvertTo(context, culture, value, destinationType);
-    //    }
-
-    //    #endregion
-    //}
-
-    //[TypeConverter(typeof(MouseStateConverter))]
-    //public class ObjectMouseStateWrapper
-    //{
-    //    #region Constructors
-
-    //    public ObjectMouseStateWrapper()
-    //    {
-    //    }
-
-    //    public ObjectMouseStateWrapper(string value)
-    //    {
-    //        Value = value;
-    //    }
-
-    //    #endregion
-
-    //    #region Properties
-
-    //    public object Value { get; set; }
-
-    //    #endregion
-    //}
 }
