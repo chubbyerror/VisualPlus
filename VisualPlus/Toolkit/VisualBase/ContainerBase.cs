65cb87ad1e61b5bbe01a6ae23666948ade38d6d3
DarkByte7 <DarkByte7@Live.com>
2017-07-06 10:42:44 -0500

    Moved methods to ContainerBase

--- VisualPlus/Toolkit/VisualBase/ContainerBase.cs
+++ VisualPlus/Toolkit/VisualBase/ContainerBase.cs
@@ -11,8 +11,8 @@
 
     using VisualPlus.Enums;
     using VisualPlus.Framework;
-    using VisualPlus.Framework.Handlers;
     using VisualPlus.Framework.Structure;
+    using VisualPlus.Toolkit.Controls;
 
     #endregion
 
@@ -85,20 +85,34 @@
 
         #region Events
 
+        /// <summary>Apply BackColor change on the container and it's child controls.</summary>
+        /// <param name="container">The container control.</param>
+        /// <param name="backgroundColor">The container backgroundColor.</param>
+        protected virtual void ApplyContainerBackColorChange(Control container, Color backgroundColor)
+        {
+            foreach (object control in container.Controls)
+            {
+                if (control != null)
+                {
+                    ((Control)control).BackColor = backgroundColor;
+                }
+            }
+        }
+
         protected virtual void OnBackgroundChanged()
         {
-            ExceptionManager.ApplyContainerBackColorChange(this, _backgroundColor);
+            ApplyContainerBackColorChange(this, _backgroundColor);
             BackgroundChanged?.Invoke();
         }
 
         protected override void OnControlAdded(ControlEventArgs e)
         {
-            ExceptionManager.SetControlBackColor(e.Control, _backgroundColor, false);
+            SetControlBackColor(e.Control, _backgroundColor, false);
         }
 
         protected override void OnControlRemoved(ControlEventArgs e)
         {
-            ExceptionManager.SetControlBackColor(e.Control, _backgroundColor, true);
+            SetControlBackColor(e.Control, _backgroundColor, true);
         }
 
         protected override void OnMouseHover(EventArgs e)
@@ -113,6 +127,32 @@
             base.OnMouseLeave(e);
             MouseState = MouseStates.Normal;
             Invalidate();
+        }
+
+        /// <summary>Set's the container controls BackColor.</summary>
+        /// <param name="control">Current control.</param>
+        /// <param name="backgroundColor">Container background color.</param>
+        /// <param name="onControlRemoved">Control removed?</param>
+        protected virtual void SetControlBackColor(Control control, Color backgroundColor, bool onControlRemoved)
+        {
+            Color backColor;
+
+            if (onControlRemoved)
+            {
+                backColor = Color.Transparent;
+
+                // Bug: The Control doesn't support transparent background
+                if (control is VisualProgressIndicator)
+                {
+                    backColor = SystemColors.Control;
+                }
+            }
+            else
+            {
+                backColor = backgroundColor;
+            }
+
+            control.BackColor = backColor;
         }
 
         #endregion
