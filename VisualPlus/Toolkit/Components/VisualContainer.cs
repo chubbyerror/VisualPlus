1936f0b14fd702645021b2e6932c0d0c3951b8da
DarkByte7 <DarkByte7@Live.com>
2017-07-11 00:10:02 -0500

    Moved VisualContainer to Components folder

--- VisualPlus/Toolkit/Components/VisualContainer.cs
+++ VisualPlus/Toolkit/Components/VisualContainer.cs
@@ -1,4 +1,4 @@
-﻿namespace VisualPlus.Toolkit.Controls
+﻿namespace VisualPlus.Toolkit.Components
 {
     #region Namespace
 
@@ -10,15 +10,16 @@
 
     #endregion
 
+    /// <summary>The visualContainer component.</summary>
     [ToolboxItem(false)]
-    [Description("The Visual Container")]
+    [Description("The Visual Container Component")]
     public sealed partial class VisualContainer : ToolStripDropDown
     {
         #region Variables
 
-        private bool m_fade = true;
-        private ToolStripControlHost m_host;
-        private Control mContextContainer;
+        private ToolStripControlHost _controlHost;
+        private bool _fade = true;
+        private Control _userControl;
 
         #endregion
 
@@ -31,21 +32,21 @@
                 throw new ArgumentNullException("No context control to load." + nameof(contextControl));
             }
 
-            mContextContainer = contextControl;
+            _userControl = contextControl;
 
-            m_fade = SystemInformation.IsMenuAnimationEnabled && SystemInformation.IsMenuFadeEnabled;
+            _fade = SystemInformation.IsMenuAnimationEnabled && SystemInformation.IsMenuFadeEnabled;
 
             // Setup control
-            m_host = new ToolStripControlHost(contextControl)
+            _controlHost = new ToolStripControlHost(contextControl)
                 {
                     AutoSize = false
                 };
 
-            Padding = Margin = m_host.Padding = m_host.Margin = Padding.Empty;
+            Padding = Margin = _controlHost.Padding = _controlHost.Margin = Padding.Empty;
 
             contextControl.Location = Point.Empty;
 
-            Items.Add(m_host);
+            Items.Add(_controlHost);
 
             contextControl.Disposed += delegate
                 {
@@ -77,14 +78,14 @@
 
         protected override void OnOpened(EventArgs e)
         {
-            mContextContainer.Focus();
+            _userControl.Focus();
 
             base.OnOpened(e);
         }
 
         protected override void OnOpening(CancelEventArgs e)
         {
-            if (mContextContainer.IsDisposed || mContextContainer.Disposing)
+            if (_userControl.IsDisposed || _userControl.Disposing)
             {
                 e.Cancel = true;
                 return;
@@ -102,33 +103,33 @@
         protected override void SetVisibleCore(bool visible)
         {
             double opacity = Opacity;
-            if (visible && m_fade)
+            if (visible && _fade)
             {
                 Opacity = 0;
             }
 
             base.SetVisibleCore(visible);
-            if (!visible || !m_fade)
+            if (!visible || !_fade)
             {
                 return;
             }
 
-            for (var i = 1; i <= frames; i++)
+            for (var i = 1; i <= _frames; i++)
             {
                 if (i > 1)
                 {
-                    Thread.Sleep(frameduration);
+                    Thread.Sleep(_frameDuration);
                 }
 
-                Opacity = (opacity * i) / frames;
+                Opacity = (opacity * i) / _frames;
             }
 
             Opacity = opacity;
         }
 
-        private const int frameduration = totalduration / frames;
-        private const int frames = 5;
-        private const int totalduration = 100;
+        private const int _frameDuration = _totalDuration / _frames;
+        private const int _frames = 5;
+        private const int _totalDuration = 100;
 
         private void Show(Control control, Rectangle area)
         {
